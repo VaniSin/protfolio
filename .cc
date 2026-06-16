1e890c3fee74dd4c0e9080d68e6b4c6d557151b1
vanis <vanis@localhost>
2026-06-16 11:45:30 +0530

    Initial commit

---
+++ .cc
@@ -0,0 +1,7 @@
+portfolio-website/  
+│── index.html  (Main Page)  
+│── style.css  (CSS Styling)  
+│── script.js  (JavaScript, if needed)  
+│── images/  (Store your images here)  
+│── projects/  (Project files, if needed)  
+│── assets/  (Fonts, icons, etc.)  
